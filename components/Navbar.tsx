4a52b93d2190ded71fd163120c3eb510f51b34d6
hanbyeol kwon <janviertrois@naver.com>
2023-12-11 10:48:34 +0900

    Implemented navbar component with framer motion

---
+++ components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { logo } from '@/public/assets';
+import Image from 'next/image';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+const Navbar = () => {
+  const navItems = [
+    ['About', '#about'],
+    ['Project', '#project'],
+    ['Contact', '#contact'],
+  ];
+
+  return (
+    <div className='w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4'>
+      <div className='max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-center'>
+        {/* <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.1 }}>
+          <Image className='w-14' src={logo} alt='logo' />
+        </motion.div> */}
+        <div>
+          <ul className='flex text-[13px] gap-7'>
+            <Link
+              href='#home'
+              className='flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.1 }}
+              >
+                Home
+              </motion.li>
+            </Link>
+            {navItems.map(([title, url], i) => (
+              <Link
+                key={title}
+                href={url}
+                className='flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link'
+              >
+                <motion.li
+                  initial={{ y: -10, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.1, delay: 0.15 * (i + 1) }}
+                >
+                  <span>0{i + 1}.</span>{title}
+                </motion.li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
