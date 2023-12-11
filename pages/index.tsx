e1b72497c9b98958e750885be7ebd02d51248577
hanbyeol kwon <janviertrois@naver.com>
2023-12-11 09:52:34 +0900

    Styled main

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main className="font-bodyFont">Portfolio</main>
+    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+      <Navbar />
+    </main>
   )
 }
