0ca900975b4bb62fbae6a015c852200901d24bce
hanbyeol kwon <janviertrois@naver.com>
2023-12-11 22:23:47 +0900

    Applied flexbox properties to content div

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Project from "@/components/Project";
+import Skill from "@/components/Skill";
 import Head from "next/head";
 
 export default function Home() {
@@ -7,8 +10,13 @@ export default function Home() {
       <Head>
         <title>Hanbyeol Kwon | Full-stack Developer</title>
       </Head>
-      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll -y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
         <Navbar />
+        <div className="w-full flex flex-col items-center mx-auto px-4">
+          <Hero />
+          <Skill />
+          <Project />
+        </div>
       </main>
     </>
   )
