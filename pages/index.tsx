cf0561bb052052e99e07ed45e39d7146e6669db0
hanbyeol kwon <janviertrois@naver.com>
2023-12-11 11:23:27 +0900

    Added head title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
-      <Navbar />
-    </main>
+    <>
+      <Head>
+        <title>Hanbyeol Kwon | Full-stack Developer</title>
+      </Head>
+      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+        <Navbar />
+      </main>
+    </>
   )
 }
