a9069b182c5afa163fa182f769d80bd7831cb3b1
hanbyeol kwon <janviertrois@naver.com>
2023-12-12 17:47:39 +0900

    Added contact component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Project from "@/components/Project";
@@ -12,10 +13,11 @@ export default function Home() {
       </Head>
       <main className="w-full font-bodyFont bg-bodyColor text-textLight">
         <Navbar />
-        <div className="w-full flex flex-col items-center mx-auto px-4">
+        <div className="w-full mx-auto flex flex-col px-4">
           <Hero />
           <Skill />
           <Project />
+          <Contact />
         </div>
       </main>
     </>
