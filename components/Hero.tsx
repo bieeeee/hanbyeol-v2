5308cefadb6f9cf6ada6de6c945bf85df4fae773
hanbyeol kwon <janviertrois@naver.com>
2023-12-12 14:28:08 +0900

    Changed framer motion from letters to words, applied textdark and text-xl to less important words

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,44 +1,36 @@
 import { motion } from 'framer-motion';
-import SectionTitle from './SectionTitle';
 
 const Hero = () => {
-  const line1 = "Hi, my name is Hanbyeol Kwon. I'm a passionate developer who appreciates clean code and aesthetic UI design. Currently, I'm focused on learning performance optimization techniques and exploring new technologies.";
-  const sentence = {
-    hidden: { opacity: 1 },
-    visible: {
-      opacity: 1,
-      transition: {
-        delay: 1,
-        staggerChildren: 0.03,
-      }
-    }
-  }
-
-  const letter = {
-    hidden: { opacity: 0, y: 50 },
-    visible: {
-      opacity: 1,
-      y: 0,
-    }
-  }
+  const words = ["Hi, my name is ", "Hanbyeol Kwon", ". I'm a ", "passionate developer", " who appreciates ", "clean code", " and ", "aesthetic UI design", ". Currently, I'm focused on learning ", "performance optimization techniques", " and exploring ", "new technologies."];
   return (
-    <section
+    <motion.section
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.3 }}
       id='about'
       className='max-w-contentContainer flex mx-auto h-[85vh] lg:h-[91vh] py-10 mdl:py-24 mdl:px-10 xl:px-4'
     >
       <div className="flex items-center justify-start">
-        <SectionTitle titleNo='01' title='About Me' className='rotate-90 whitespace-nowrap text-base w-[100px]' />
-        <div className="max-w-[800px] mx-auto text-4xl font-bold text-left">
-          <motion.h1 variants={sentence} initial="hidden" animate="visible">
-            {line1.split("").map((char, i) => (
-              <motion.span key={char + "-" + i} custom={i} variants={letter}>
-                {char}
-              </motion.span>
+        <div className="max-w-[800px] mx-auto text-4xl font-bold text-center">
+          <div>
+            {words.map((word, i) => (
+              (i % 2 === 0) ?
+                <h1 key={word} className='inline text-textDark text-xl'>{word}</h1>
+                :
+                <motion.h1
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 0.35 * (i) }}
+                  key={word}
+                  className='inline'
+                >
+                  {word}
+                </motion.h1>
             ))}
-          </motion.h1>
+          </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   )
 }
 
