8e6863069bfb612b4567780f4232a34150e0f66e
hanbyeol kwon <janviertrois@naver.com>
2023-12-13 11:01:38 +0900

    Initialized project items for project card component

---
+++ components/ProjectItem.tsx
@@ -0,0 +1,14 @@
+export const ProjectItem = [
+  {
+    id: "v1",
+    title: "Hanbyeol-v1",
+    desc: "sfdsfdsasdfdsfa",
+    stack: ["React", "Express", "Heroku"],
+  },
+  {
+    id: "v2",
+    title: "Hanbyeol-v2",
+    desc: "sfdsfdsasdfdfsdfasdfsfa",
+    stack: ["Next", "TailwindCSS", "TypeScript"],
+  }
+]
