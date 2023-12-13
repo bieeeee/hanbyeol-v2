0d37ed44064ed4a6472102c2a19c28cab5de95f0
hanbyeol kwon <janviertrois@naver.com>
2023-12-13 15:14:29 +0900

    Changed bg overlay color to green, adjusted div layout, added project details

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from "react-dom";
 import { ProjectModalItem } from "./ProjectModalItem";
 import { IoClose } from "react-icons/io5";
-import Image from "next/image";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { FaCode } from "react-icons/fa6";
+import { RiArrowDropRightLine } from 'react-icons/ri';
 
 interface ModalProps {
   open: boolean;
@@ -11,21 +14,41 @@ interface ModalProps {
 
 const Modal = ({ open, onClose, project }: ModalProps) => {
   if (!open) return null;
-  const thisProject = ProjectModalItem.find((item)=>(item.id === project));
-  const imageURL = thisProject?.image;
+  const thisProject = ProjectModalItem.find((item) => (item.id === project));
 
   return ReactDOM.createPortal(
     <>
       <div key={thisProject?.id} className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
-        <div className="max-w-containerxs relative w-auto my-6 mx-auto">
-          <div className="w-10/12 rounded-md relative flex flex-col w-full bg-white mx-auto">
-              <button className="absolute top-0 right-0 p-2 text-xl x-10 z-10" onClick={onClose}>
-                  <IoClose />
-              </button>
-            <div className="relative p-6 flex flex-col items-center justify-center">
-              <div className="">{thisProject?.image}</div>
-              <div className="">dd</div>
+        <div className="modal-overlay absolute w-full h-full bg-textGreen opacity-10"></div>
+        <div className="max-w-3xl relative w-auto my-6 mx-auto px-6">
+          <div className="rounded relative flex flex-col w-full mx-auto bg-bodyColor">
+            <button className="absolute top-0 right-0 p-2 text-2xl x-10 z-10 text-textGreen" onClick={onClose}>
+              <IoClose />
+            </button>
+            <div className="relative p-9 text-textLight">
+              <p className="mb-3">{thisProject?.image}</p>
+              <p className="ml-1 text-2xl font-titleFont font-bold tracking-wide text-textGreen text-center">{thisProject?.title}</p>
+              <div className="ml-1 text-xs tracking-wide flex items-end gap-3 mb-3 px-0.5 text-textDark justify-center">
+                <div className="flex gap-1 items-center">
+                  <BsFillPeopleFill size={13} />
+                  {thisProject?.team}
+                </div>
+                <div className="flex items-center gap-1">
+                  <FaRegCalendarAlt size={13} />
+                  {thisProject?.period}
+                </div>
+                <div>
+                  <a href={thisProject?.gh} target="_blank">
+                    <FaCode size={15} className="text-textGreen" />
+                  </a>
+                </div>
+              </div>
+              {thisProject?.role.map((item, i) => (
+                <div key={i} className="flex items-start mb-2 text-xs sml:text-base md:text-lg">
+                  <p><RiArrowDropRightLine className="text-textGreen" size={27} /></p>
+                  <p className="leading-snug">{item}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
