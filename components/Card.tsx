edff75abb6cf5228be3a521ca9f5033cf532977a
hanbyeol kwon <janviertrois@naver.com>
2023-12-13 15:10:00 +0900

    Devided card into 2 divs, changed description font-size to text-sm

--- components/Card.tsx
+++ components/Card.tsx
@@ -18,22 +18,24 @@ export const Card = ({ project }) => {
 
   return (
     <>
-      <div key={project.id} className='w-72 h-72 rounded-lg bg-[#112240] p-7 flex flex-col justify-center gap-6 hover:-translate-y-2 transition-transform duration-300 group cursor-pointer' onClick={() => handleClick(project)}>
-        <FaRegFolder className="text-4xl text-textGreen" />
-        <div className="">
+      <div className='w-72 h-72 rounded-lg bg-[#112240] p-7 flex flex-col items-start justify-between gap-6 hover:-translate-y-2 transition-transform duration-300 group cursor-pointer' onClick={() => handleClick(project)}>
+        <div>
+          <FaRegFolder className="text-4xl text-textGreen mb-2" />
           <h2 className='flex gap-2 items-center text-xl font-titleFont font-semibold tracking-wide group-hover:text-textGreen'>
             {project.title}
             <HiMiniMagnifyingGlassPlus className='hidden group-hover:block' />
           </h2>
-          <p className='text-small mt-3'>
+          <p className='text-sm mt-3'>
             {project.desc}
           </p>
         </div>
-        <ul className='test-xs mdl:text-sm text-textDark flex items-center gap-4 flex-wrap'>
-          {project.stack.map((stack: string, i: number) => (
-            <li key={i}>{stack}</li>
-          ))}
-        </ul>
+        <div>
+          <ul className='test-xs mdl:text-sm text-textDark flex items-center gap-4 flex-wrap'>
+            {project.stack.map((stack: string, i: number) => (
+              <li key={i}>{stack}</li>
+            ))}
+          </ul>
+        </div>
       </div>
       {isModalOpen && <Modal open={isModalOpen} onClose={handleClose} project={selectedProject} />}
     </>
