cf556701623df0cf9baf85f8309e65d308fea23c
hanbyeol kwon <janviertrois@naver.com>
2023-12-13 15:10:44 +0900

    Applied my to hamburger navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-        <div className='w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group ml-auto'>
+        <div className='w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group ml-auto my-5'>
           <span className='w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300'></span>
           <span className='w-full h-[2px] bg-textGreen inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all ease-in-out duration-300'></span>
           <span className='w-full h-[2px] bg-textGreen inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all ease-in-out duration-300'></span>
