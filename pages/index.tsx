6f752e4eaa83fd16a28b83471d7f41439c5ebb18
hanbyeol kwon <janviertrois@naver.com>
2023-12-13 19:23:50 +0900

    Added ssr false to dynamic import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import Contact from "@/components/Contact";
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Project from "@/components/Project";
-import Skill from "@/components/Skill";
+import dynamic from 'next/dynamic';
 import Head from "next/head";
+const Navbar = dynamic(() => import("@/components/Navbar"));
+const Hero = dynamic(() => import("@/components/Hero"));
+const Skill = dynamic(() => import("@/components/Skill"), { ssr: false });
+const Project = dynamic(() => import("@/components/Project"), { ssr: false });
+const Contact = dynamic(() => import("@/components/Contact"), { ssr: false });
 
 export default function Home() {
   return (
