790783a4a7c06197319a480eba710287ee0a0ec6
hanbyeol kwon <janviertrois@naver.com>
2023-12-14 18:19:29 +0900

    Changed framer-motion

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,34 +1,42 @@
-const Hero = ({m}:any) => {
+const Hero = ({ m }: any) => {
   const words = ["Hi, my name is ", "Hanbyeol Kwon", ". I'm a ", "passionate developer", " who appreciates ", "clean code", " and ", "aesthetic UI design", ". Currently, I'm focused on learning ", "performance optimization techniques", " and exploring ", "new technologies."];
   return (
-      <m.section
-        initial={{ y: 10, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.3 }}
-        id='about'
-        className='max-w-contentContainer h-[89vh] flex mx-auto mdl:px-10 xl:px-4'
-      >
-        <div className="flex items-center justify-start">
-          <div className="max-w-[800px] mx-auto text-4xl font-bold text-center">
-            <div>
-              {words.map((word, i) => (
-                (i % 2 === 0) ?
-                  <h1 key={word} className='inline text-textDark text-xl'>{word}</h1>
-                  :
-                  <m.h1
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.5, delay: 0.35 * (i) }}
-                    key={word}
-                    className='inline'
-                  >
-                    {word}
-                  </m.h1>
-              ))}
-            </div>
+    <m.section
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.3 }}
+      id='about'
+      className='max-w-contentContainer h-[89vh] flex mx-auto mdl:px-10 xl:px-4'
+    >
+      <div className="flex items-center justify-start">
+        <div className="max-w-[800px] mx-auto text-5xl font-bold text-center">
+          <div>
+            {words.map((word, i) => (
+              (i % 2 === 0) ?
+                <m.h1
+                  key={word}
+                  className='inline text-textDark'
+                  initial={{ fontSize: "3rem", color: "#e8eeff" }}
+                  animate={{ fontSize: "1.25rem", color: "#8892b0" }}
+                  transition={{ duration: 0.3, delay: 5.1 }}
+                >
+                  {word}
+                </m.h1>
+                :
+                <m.h1
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 0.35 * (i) }}
+                  key={word}
+                  className='inline text-[#e8eeff]'
+                >
+                  {word}
+                </m.h1>
+            ))}
           </div>
         </div>
-      </m.section>
+      </div>
+    </m.section>
   )
 }
 
